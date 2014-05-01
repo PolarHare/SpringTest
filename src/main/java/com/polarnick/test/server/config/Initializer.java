18d385b585df852318e5018e5744856467596871
Nikita <nikzyul@ya.ru>
2014-05-01 15:05:31 +0400

    annotation config initialization

--- src/main/java/com/polarnick/test/server/config/Initializer.java
+++ src/main/java/com/polarnick/test/server/config/Initializer.java
@@ -1,13 +1,6 @@
 package com.polarnick.test.server.config;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
  * http://kielczewski.eu/2013/11/spring-mvc-without-web-xml-using-webapplicationinitializer/
@@ -15,16 +8,19 @@ import javax.servlet.ServletRegistration;
  *
  * @author Polyarnyi Nikolay
  */
-public class Initializer implements WebApplicationInitializer {
-
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.setConfigLocation("com.polarnick.test.server.config");
-        servletContext.addListener(new ContextLoaderListener(context));
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("MyDispatcherServlet", new DispatcherServlet(context));
-        dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
     }
 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { AppConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebMvcConfig.class, WebSecurityConfig.class };
+    }
 }
